b5146fdd32bb3aab3ecd31caa71f18e6c65d727b
achmad abdul aziz <achmad.a@elabram.com>
2021-12-05 16:18:04 +0700

    Initial Commit

---
+++ Postings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Postings
+//
+//  Created by Elabram MacbookPro on 05/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
